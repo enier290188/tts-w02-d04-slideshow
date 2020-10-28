c2e2ba413a7763f2fb97d9c941bfcbc316d90374
Enier Ramos García <enier290188@gmail.com>
2020-10-28 14:59:31 -0400

    Adding "index.js" file.

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+    Create a single object named "slideshow" that represents the data and functionality of a picture slideshow.
+    There should be NO VARIABLES OUTSIDE THE OBJECT.
+    The object should have properties for:
+*/
+
