91ad3b9356df9362251adf885a73ca5c45ba6678
Enier Ramos García <enier290188@gmail.com>
2020-10-28 15:43:35 -0400

    7. A play() function that moves to the next photo ever 2000ms until the end. Tip - use playInterval = setInterval(fn,ms).

--- index.js
+++ index.js
@@ -37,4 +37,12 @@ let slideshow = {
     },
     // 6. Create an empty property named playInterval.
     playInterval: null,
+    // 7. A play() function that moves to the next photo ever 2000ms until the end. Tip - use playInterval = setInterval(fn,ms).
+    play: function () {
+        this.playInterval = setInterval(function () {
+            this.nextPhoto()
+        }.bind(this), 2000);
+    },
 }
+
+slideshow.play();
